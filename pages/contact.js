ac2cfe3dbce9845af2262944c3a7905f0b3b58ce
Admin <admin@Addapps-MacBook-Pro.local>
2021-01-25 19:10:24 -0800

    WIP

--- pages/contact.js
+++ pages/contact.js
@@ -11,6 +11,7 @@ import {
   FormText,
   Row,
   Col,
+  FormFeedback
 } from "reactstrap";
 
 const Contact = () => {
@@ -21,27 +22,47 @@ const Contact = () => {
       <BaseLayout user={data} loading={loading}>
         <BasePage>
           <Row>
-            <Col md="8">
-              <h3>(919) 771-7412</h3>
-              <h3>mcct@smcsd.org</h3>
+            <Col md="6">
+              <h3><a href="tel:+15109562978">510 956 2978</a></h3>
+              <h3><a href="mailto:mcct@smcsd.org">mcct@smcsd.org</a></h3>
               <h3>Marin City, CA 94965</h3>
             </Col>
-          </Row>
+
+          <Col md="6"> 
           <Form>
-            <FormGroup>
-              <Label>First Name</Label>
-              <Input></Input>
-            </FormGroup>
-            <FormGroup>
-              <Label>Last Name</Label>
-              <Input></Input>
-            </FormGroup>
+            <Row form>
+            <Col md="6">
+              <FormGroup>
+                <Label>First Name</Label>
+                <Input type="first name" name="firstName" id="firstName" placeholder="Joan" />
+                <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+              </FormGroup>
+            </Col>
+            <Col md="6">
+              <FormGroup>
+                <Label>Last Name</Label>
+                <Input type="last name" name="lastName" id="lastName" placeholder="Smith" />
+              </FormGroup>  
+            </Col>
+            </Row>
+
             <FormGroup>
               <Label>Email</Label>
-              <Input></Input>
+              <Input type="email" name="email" id="Email" placeholder="Email Address" />
+            </FormGroup>
+            <FormGroup>
+              <Label>Phone</Label>
+              <Input type="phone" name="phone" id="phone" placeholder="510-555-5555" />
+            </FormGroup>
+            <FormGroup >
+              <Label for="exampleText" >Messege </Label>
+                <Input type="textarea" name="text" id="text" placeholder="Enter your message" />
             </FormGroup>
             <Button>Submit </Button>
           </Form>
+          </Col>
+          </Row>
+
         </BasePage>
       </BaseLayout>
     </>
--- pages/leadership.js
+++ pages/leadership.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
-import { useGetUser } from "@/actions/user";
+import useGetUser from "@/actions/user";
 import { Row, Col } from "reactstrap";
 
 const Leadership = () => {
@@ -27,61 +27,23 @@ const Leadership = () => {
         <Row className="mt-5 align-items-center top-bio">
           <Col md="6">
             <div className="left-side">
-              <img src="https://images.squarespace-cdn.com/content/v1/5ebaf8af593d7e00d9fda34c/1610485075298-9UX37O11MIZYA9S9Y008/ke17ZwdGBToddI8pDm48kC3u7xvksWcmaMl56WdA9mAUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2Fy4RluPhWCm33cru5FUyz20XV34uBtEn3xovmaxOR8SbtxPRH917Fz9Qv_NunPol/jahmeer.jpg?format=260w" />
-              <h2 className={`title ${createFadeInClass()}`}>
+              <img  width="100%" src="https://images.squarespace-cdn.com/content/v1/5ebaf8af593d7e00d9fda34c/1610485075298-9UX37O11MIZYA9S9Y008/ke17ZwdGBToddI8pDm48kC3u7xvksWcmaMl56WdA9mAUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2Fy4RluPhWCm33cru5FUyz20XV34uBtEn3xovmaxOR8SbtxPRH917Fz9Qv_NunPol/jahmeer.jpg?format=500w" />
+              
+            </div>
+          </Col>
+          <Col md="6">
+          <h2 className={`title ${createFadeInClass()}`}>
                 Jahmeer Reynolds{" "}
               </h2>
               <p className={`subsubTitle ${createFadeInClass()}`}>
                 Executive Director
               </p>
-            </div>
-          </Col>
-          <Col md="6">
             <div className={`${createFadeInClass()}`}>
               <p>
-                Jahmeer Reynolds was born in Trenton, NJ and raised primarily by
-                his grandmother, along with his parents and a host of relatives.
-                Jahmeer was a latch key kid, who knew from an early age that
-                education was his ticket to something greater. He loved to read
-                and he excelled at running. His talent as an athlete allowed him
-                to travel the world before completing high school. After
-                graduating from Trenton Central High School, Jahmeer left New
-                Jersey to attend college in Raleigh, North Carolina.
+              Jahmeer Reynolds was born in Trenton, NJ and raised primarily by his grandmother, along with his parents and a host of relatives. Jahmeer was a latch key kid, who knew from an early age that education was his ticket to something greater.  He loved to read and he excelled at running.  His talent as an athlete allowed him to travel the world before completing high school.  After graduating from Trenton Central High School, Jahmeer left New Jersey to attend college in Raleigh, North Carolina.{" "}
               </p>
               <p>
-                Jahmeer had to overcome many struggles, but he never gave up and
-                eventually earned his bachelor's degree in Business
-                Administration in 2007. Jahmeer has always had a strong desire
-                to serve his community, and worked his way through college as an
-                advocate for adults and children with mild to severe
-                developmental, physical and mental disabilities. While pursuing
-                his degree, Jahmeer became a member of the illustrious Kappa
-                Alpha Psi Fraternity, Inc., as well as an accomplished
-                playwright. In 2004, Jahmeer wrote and directed a stage play
-                about the AIDS epidemic, and toured around the country. This
-                lead to Jahmeer becoming a board member of the organization,
-                African Advocates Against Aids in North Carolina. Jahmeer met
-                his wife Shakira in 2010, and he began to plan for a career as a
-                special education teacher. They were married in 2011, and
-                Jahmeer became an educator in the Wake County Public School
-                system.
-              </p>
-              <p>
-                In 2012, Jahmeer and his family relocated to Abu Dhabi. Since
-                then, Jahmeer has earned his Masters in Education and in 2015
-                launched the I Promise Mentoring Program to provide support and
-                mentorship to expat young African American men living in the
-                United Arab Emirates. The I Promise program includes a number of
-                components including financial literacy classes, social media
-                etiquette, human growth and development, community excursions,
-                and one on one mentoring. Jahmeer is currently working towards
-                his doctoral degree in Educational Psychology. Supported by his
-                wife and many of his closest friends, Jahmeer has seen IPromise
-                grow and thrive. For his international efforts Jahmeer was
-                awarded the Blue Sapphire Award for his international community
-                efforts. Of his many accomplishments, Jahmeer is most proud of
-                being a husband and father, and values time with his family
-                above all else.
+              In 2012, Jahmeer and his family relocated to Abu Dhabi.  Since then, Jahmeer has earned his Masters in Education and in 2015 launched the I Promise Mentoring Program to provide support and mentorship to expat young African American men living in the United Arab Emirates. The  I Promise program includes a number of components including financial literacy classes, social media etiquette, human growth and development, community excursions, and one on one mentoring.  Jahmeer is currently working towards his doctoral degree in Educational Psychology. Supported by his wife and many of his closest friends, Jahmeer has seen IPromise grow and thrive. For his international efforts Jahmeer was awarded the Blue Sapphire Award for his international community efforts. Of his many accomplishments, Jahmeer is most proud of being a husband and father, and values time with his family above all else.  
               </p>
             </div>
           </Col>
@@ -89,29 +51,18 @@ const Leadership = () => {
         <Row className="mt-5 align-items-center bottom-bio">
           <Col md="6">
             <div className="left-side justify-content-center">
-              <img src="https://images.squarespace-cdn.com/content/v1/5ebaf8af593d7e00d9fda34c/1589424889858-02WGI5KERF5H3LZLV006/ke17ZwdGBToddI8pDm48kG6E2OWZRwBlbeokYOUL6KpZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwB8gR31QkEvV5Cnzs01oeB_kT3VSav0P2RK0WGwSxdiE41Z8N2AkAYi6v8kXrG5A4/mr+phil.jpg?format=260w" />
-              <h2 className={`title ${createFadeInClass()}`}>Phillip Logan</h2>
-              <p className={`subsubTitle ${createFadeInClass()}`}>
-                Intake Supervisor
-              </p>
+              <img width="100%" src="https://images.squarespace-cdn.com/content/v1/5ebaf8af593d7e00d9fda34c/1589424889858-02WGI5KERF5H3LZLV006/ke17ZwdGBToddI8pDm48kG6E2OWZRwBlbeokYOUL6KpZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwB8gR31QkEvV5Cnzs01oeB_kT3VSav0P2RK0WGwSxdiE41Z8N2AkAYi6v8kXrG5A4/mr+phil.jpg?format=260w" />
+              
             </div>
           </Col>
           <Col md="6">
+          <h2 className={`title ${createFadeInClass()}`}>Phillip Logan</h2>
+              <p className={`subsubTitle ${createFadeInClass()}`}>
+                Intake Supervisor
+              </p>
             <div className={`${createFadeInClass()}`}>
               <p>
-                A Vallejo native, has prided himself on being a dual son of the
-                Marin City community. For his entire life, his father has been a
-                minister in Marin City, sparking Phillip's drive to community
-                service. He has spent the last 8 years of his life serving youth
-                in Marin City, starting off as a contractor in the Hannah
-                Project Partnership for Academic Achievement, a local community
-                based organization, eventually moving on to serve as a Student
-                Support Specialist and Music Teacher at Willow Creek Academy. He
-                now serves as the Music Instructor and Student and Family
-                Support Intern at Bayside Martin Luther King Jr. Academy. As
-                highlighted in his signature phrase "Peace and Prosperity",
-                Phillip believes that true peace and wealth comes from the
-                internal satisfaction of serving others.
+              A Vallejo native, has prided himself on being a dual son of the Marin City community. For his entire life, his father has been a minister in Marin City, sparking Phillip's drive to community service. He has spent the last 8 years of his life serving youth in Marin City, starting off as a contractor in the Hannah Project Partnership for Academic Achievement, a local community based organization, eventually moving on to serve as a Student Support Specialist and Music Teacher at Willow Creek Academy. He now serves as the Music Instructor and Student and Family Support Intern at Bayside Martin Luther King Jr. Academy. As highlighted in his signature phrase "Peace and Prosperity", Phillip believes that true peace and wealth comes from the internal satisfaction of serving others. 
               </p>
             </div>
           </Col>
